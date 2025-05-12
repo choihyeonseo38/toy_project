456589e1ce9696d5ee7ad2f5edc5e476d659a8c3
choihyeonseo38 <porroriiiii11@gmail.com>
2025-05-12 19:18:27 +0900

    toy_projrct

--- toy_project/harugachi/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-// src/global.d.ts
-declare global {
-    interface Window {
-      kakao: any; // kakao 타입을 any로 설정
-    }
-  }
-  
-  export {}; // 이 줄이 필요합니다.
--- toy_project/harugachi/src/redux/types.ts
+++
@@ -1,7 +0,0 @@
-// src/redux/types.ts
-
-import { Post } from '../types'; // 공용 타입 임포트
-
-export interface PostState {
-  posts: Post[]; // Post 타입 배열
-}
